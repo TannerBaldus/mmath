efc1da90a0a6dd8f53fb6759b848ce30aed12800
Tanner Baldus <tanner@ridecell.com>
2015-11-16 17:30:24 -0800

    Attempt to get image from sherdog if not availble on espn

--- fighter_scraper.js
+++ fighter_scraper.js
@@ -29,31 +29,69 @@ function historyUrl(url){
     return "http://espn.go.com/mma/fighter/history/_/id/"+getID(url);
 }
 
-function fighter(url_end, callback){
+function sherdogImage(fighter, callback){
+    
+    if(!fighter.img){
+        var sherdogUrl = "http://www.sherdog.com/stats/fightfinder?SearchTxt="+fighter.name.split(' ').join('+');
+        request(sherdogUrl, function(error, response, html){
+            var $ = cheerio.load(html);
+            if (!error && response.statusCode == 200){
+                var span = $('span:contains("Fighter Results List")', html);
+                if(span){
+                    $('.fightfinder_result tr:not(".table_head")').each(function(i, val){
+                        var tds = $(val).find('td');
+                        var nameLink = tds.eq(1).find('a')
+                        var nickname = tds.eq(2);
+                        if(fighter.name == nameLink.text() && '"'+fighter.nickname+'"' == nickname){
+                            var sherdogFighterUrl = "http://www.sherdog.com"+$(nameLink).attr('href');
+                            request(sherdogFighterUrl, function(error, response, html){
+                                if(!error && response.statusCode == 200){
+                                    console.log('wtf mate');
+                                    fighter.img = $('img .profile_image',html);
+                                    callback(fighter)
+
+                                }
+                                else{callback(fighter)}
+                            })
+                        }
+                        else{callback(fighter)}
+                    })
+                }
+                else{callback(fighter)}
+                
+            }
+            else{callback(fighter)}
+        
+        });
+    }
+    else{callback(fighter);}
+    
+    
+}
+
+function fighter(url_end, secondary_serach, callback){
     url = historyUrl(url_end);
     request(url, function(error, response, html){
     	if (!error && response.statusCode == 200) {
     	    var $ = cheerio.load(html);
     	    isWin = function(tr){return $(tr).find('td').eq(3).text()=='Win';}
-    	    var fighter = { name: "",
-    			    nickname: "",
-    			    wins : [],
-    			    id: "",
+    	    var fighter = { 
+                 name: "",
+    			 nickname: "",
+    			 wins : [],
+    			 id: "",
 			    img: "",
-    		};
-	    image = $(".main-headshot img").attr("src");
-	    if(!image)
-		;
-    	    fighter.id = getID(url);
-    	    fighter.name = $('.mod-content h1').html();
-    	    fighter.nickname = $(".player-metadata li:contains('Nickname')").contents().eq(1).text();
-    	    $('.evenrow, .oddrow').each(function(i, val){
-		if(isWin($(val))){
-		    console.log('foo');
-		    fighter.wins.push(parseWin($(val)));
-		}
-	    });
-    	    callback(fighter);
+            };
+            image = $(".main-headshot img").attr("src");
+            fighter.id = getID(url);
+            fighter.name = $('.mod-content h1').html();
+            fighter.nickname = $(".player-metadata li:contains('Nickname')").contents().eq(1).text();
+            $('.evenrow, .oddrow').each(function(i, val){
+                if(isWin($(val))){
+                    fighter.wins.push(parseWin($(val)));
+                }
+            });  
+            secondary_serach(fighter, callback);
     	}
 
     });
@@ -67,9 +105,9 @@ function get_fighters(fighter, callback){
 	    $('.evenrow a, .oddrow a').each(function(i, val){
 		fighter($(val).attr('href'));});
 	   }
-	}
+	});
 
 }
 
 
-fighter("http://espn.go.com/mma/fighter/history/_/id/2335639/jon-jones", function(fighter){console.log(fighter);});
+fighter("http://espn.go.com/mma/fighter/history/_/id/2335639/jon-jones", sherdogImage, function(fighter){console.log(fighter);});
