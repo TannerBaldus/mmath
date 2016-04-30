0160912ddcca2687b3a1550ced8467bf51022cc9
Tanner Baldus <tanner@Macs-MacBook-Pro.local>
2016-04-29 18:08:14 -0700

    Return promise from fighter function

--- fighter_scraper.js
+++ fighter_scraper.js
@@ -29,72 +29,37 @@ function historyUrl(url){
     return "http://espn.go.com/mma/fighter/history/_/id/"+getID(url);
 }
 
-function sherdogImage(fighter, callback){
-    
-    if(!fighter.img){
-        var sherdogUrl = "http://www.sherdog.com/stats/fightfinder?SearchTxt="+fighter.name.split(' ').join('+');
-        request(sherdogUrl, function(error, response, html){
-            var $ = cheerio.load(html);
-            if (!error && response.statusCode == 200){
-                var span = $('span:contains("Fighter Results List")', html);
-                if(span){
-                    $('.fightfinder_result tr:not(".table_head")').each(function(i, val){
-                        var tds = $(val).find('td');
-                        var nameLink = tds.eq(1).find('a')
-                        var nickname = tds.eq(2);
-                        if(fighter.name == nameLink.text() && '"'+fighter.nickname+'"' == nickname){
-                            var sherdogFighterUrl = "http://www.sherdog.com"+$(nameLink).attr('href');
-                            request(sherdogFighterUrl, function(error, response, html){
-                                if(!error && response.statusCode == 200){
-                                    console.log('wtf mate');
-                                    fighter.img = $('img .profile_image',html);
-                                    callback(fighter)
-
-                                }
-                                else{callback(fighter)}
-                            })
-                        }
-                        else{callback(fighter)}
-                    })
-                }
-                else{callback(fighter)}
-                
+function fighter(url_end){
+    var p1 = new Promise(function(resolve, reject){
+        url = historyUrl(url_end);
+        request(url, function(error, response, html){
+            if (!error && response.statusCode == 200) {
+                var $ = cheerio.load(html);
+                isWin = function(tr){return $(tr).find('td').eq(3).text()=='Win';};
+                var fighter = {
+                     name: "",
+                     nickname: "",
+                     wins : [],
+                     id: "",
+                     img: "",
+                };
+                fighter.id = getID(url);
+                fighter.name = $('.mod-content h1').html();
+                fighter.img = $(".main-headshot img").attr("src");
+                fighter.nickname = $(".player-metadata li:contains('Nickname')").contents().eq(1).text();
+                $('.evenrow, .oddrow').each(function(i, val){
+                    if(isWin($(val))){
+                        fighter.wins.push(parseWin($(val)));
+                    }
+                });
+                resolve(fighter);
             }
-            else{callback(fighter)}
-        
+            else {
+                reject(error);
+            }
         });
-    }
-    else{callback(fighter);}
-    
-    
-}
-
-function fighter(url_end, secondary_serach, callback){
-    url = historyUrl(url_end);
-    request(url, function(error, response, html){
-    	if (!error && response.statusCode == 200) {
-    	    var $ = cheerio.load(html);
-    	    isWin = function(tr){return $(tr).find('td').eq(3).text()=='Win';}
-    	    var fighter = { 
-                 name: "",
-    			 nickname: "",
-    			 wins : [],
-    			 id: "",
-			    img: "",
-            };
-            image = $(".main-headshot img").attr("src");
-            fighter.id = getID(url);
-            fighter.name = $('.mod-content h1').html();
-            fighter.nickname = $(".player-metadata li:contains('Nickname')").contents().eq(1).text();
-            $('.evenrow, .oddrow').each(function(i, val){
-                if(isWin($(val))){
-                    fighter.wins.push(parseWin($(val)));
-                }
-            });  
-            secondary_serach(fighter, callback);
-    	}
-
     });
+    return p1;
 }
 
 function get_fighters(fighter, callback){
