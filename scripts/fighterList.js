e0cf1aa7ad4532a29d899d6f9e272d248378f681
Tanner Baldus <tbaldus285@gmail.com>
2016-05-12 18:33:34 -0700

    Script for creating fighter json files for autocompete
    Since users aren't adding any data we can just store the fighters in memory for autocomplete.
    The script makes a list of ufc fighters to be consumed prefetched by the front end. And a json
    file of the form 'name':fighterobj to be used with the trie autocomplete in the backend

---
+++ scripts/fighterList.js
@@ -0,0 +1,81 @@
+var jsonfile = require('jsonfile');
+var cheerio = require('cheerio');
+var requestp = require('request-promise');
+
+
+
+/**
+* Using the rest api gets a json string of all nodes in the db
+* @return {string}  an Array instance witht the contents shuffled.
+*/
+function getNeo4jJson(){
+  var headers = {
+      'accept': 'application/json',
+      'content-type': 'application/json'
+  };
+
+  var dataString = '{"statements":[{"statement":"MATCH (n) RETURN (n)"}]}';
+
+  var options = {
+      url: 'http://localhost:7474/db/data/transaction/commit',
+      method: 'POST',
+      headers: headers,
+      body: dataString
+  };
+
+  return requestp(options);
+
+}
+
+
+/**
+* Using the rest api gets a json string of all nodes in the db
+* @return {string}  an Array instance witht the contents shuffled.
+*/
+function formatNeo4jResponse(noe4jReponse){
+  var formmatedObj= {};
+  var r= JSON.parse(noe4jReponse);
+  r.results[0].data.forEach(i => {
+    fighterObj = i.row[0];
+    if(fighterObj.name){
+      formmatedObj[fighterObj.name] = fighterObj;
+    }
+  });
+  return  formmatedObj;
+}
+
+/**
+* Creates an object of ufc fighter by filtering an array object of all Fighters in
+* the db against the wikipedia page of current ufc fighters.
+* @param {Array} allFighters an Array of fighter objects form [name:{fighterID, name, nickname, img},..]
+* @return {Array} an Array of fighter objects [{fighterID, name, nickname, img},..]
+*/
+function getUFCRoster(allFighters){
+  return requestp('https://en.wikipedia.org/wiki/List_of_current_UFC_fighters?oldformat=true')
+  .then(function(body){
+    var $ = cheerio.load(body);
+    var wikitables = $('.wikitable').slice(2);
+    var ufcArr = [];
+    wikitables.find('span.fn').each((i,v) => {
+        var fighterName = $(v).text();
+        var fighterObj = allFighters[fighterName];
+        ufcArr.push(fighterObj);
+    });
+    return ufcArr;
+  });
+}
+
+
+function main(){
+  getNeo4jJson()
+  .then(noe4jReponse => formatNeo4jResponse(noe4jReponse))
+  .then(formattedObj => {
+    jsonfile.writeFile( 'public/allFighters.json', formattedObj, e => {if(e){console.error(e);}});
+    return formattedObj;
+  })
+  .then(allFighters => getUFCRoster(allFighters))
+  .then(ufcArr => jsonfile.writeFile('public/ufcFighters.json', ufcArr, e => {if(e) console.err(err);}))
+  .catch(err => console.error(err+err.stack));
+}
+
+main();
