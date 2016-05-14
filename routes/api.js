e9f93bbcf03c3b4046b2f29902d737f951f42866
Tanner Baldus <tbaldus285@gmail.com>
2016-05-13 17:01:46 -0700

    Dedupe the fighter objects list.
    Have to do this since using nickname and last names to the tokens could bring up
    the same fighter twice. Spotted with the fighter Koreano Koreano. Which is an amazing name.
    I want to see him fight Americnao Americano.

--- routes/api.js
+++ routes/api.js
@@ -10,16 +10,25 @@ autocomplete.initialize(function(onReady) {
     onReady(Object.keys(allFighters));
 });
 
-
-
 var router = express.Router();
 
-
+function dedupFighters(fighterList){
+    var dedupedArr = [];
+    var seenIDs = {};
+    fighterList.forEach(el=>{
+        if(!seenIDs[el.fighterID]){
+            dedupedArr.push(el);
+            seenIDs[el.fighterID] = true;
+        }
+    });
+    return dedupedArr;
+}
 
 router.get('/fighters/search*', function (req, res, next){
     var searchQuery = req.query.q;
+
     var searchResults  = autocomplete.search(searchQuery);
-    var fighterObjs = searchResults.map(i => allFighters[i]);
+    var fighterObjs = dedupFighters([].concat.apply([], searchResults.map(i => allFighters[i])));
     res.send(fighterObjs);
 });
 
