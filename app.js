31470193bb5bb035b134be7713fca7ff6a744afa
Tanner Baldus <tbaldus285@gmail.com>
2016-05-22 22:19:00 -0700

    Heroku cwd fix

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+process.env.PWD = process.cwd();
 
 var routes = require('./routes/api');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,8 +22,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/bower_components',  express.static(__dirname + '/bower_components'));
+app.use(express.static(path.join(process.env.PWD, 'public')));
+app.use('/bower_components',  express.static(process.env.PWD + '/bower_components'));
 
 app.use('/api', routes);
 
